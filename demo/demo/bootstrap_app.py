8ae0ed6c599cb0e4e9a65389604697d7c494ed59
Saran33 <36115026+Saran33@users.noreply.github.com>
2022-01-26 20:22:14 +0400

    rm dash_core_components and dash_html_components
    Replaced the depreciated dash_core_components with Dash.dcc and the depreciated dash_html_components with Dash.html

--- demo/demo/bootstrap_app.py
+++ demo/demo/bootstrap_app.py
@@ -23,8 +23,10 @@ SOFTWARE.
 '''
 
 import dash
-import dash_bootstrap_components as dbc
-import dash_html_components as html
+# import dash_core_components as dcc
+from dash import dcc
+# import dash_html_components as html
+from dash import html
 
 from django_plotly_dash import DjangoDash
 
