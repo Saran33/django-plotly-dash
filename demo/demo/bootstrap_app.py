80bc8715199edfe3cae4c4b9df52c288c050fbc3
Saran33 <36115026+Saran33@users.noreply.github.com>
2022-01-26 20:24:26 +0400

    rm dash_html_components
    Replaced the depreciated dash_html_components with Dash.html

--- demo/demo/bootstrap_app.py
+++ demo/demo/bootstrap_app.py
@@ -23,8 +23,7 @@ SOFTWARE.
 '''
 
 import dash
-# import dash_core_components as dcc
-from dash import dcc
+import dash_bootstrap_components as dbc
 # import dash_html_components as html
 from dash import html
 
